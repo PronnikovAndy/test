1397cdb2ff17dfe3b17cfd3855529a154ac9e9c5
Kerim <kerim@ScorpAIs-iMac.local>
2020-12-08 21:57:59 +0000

    video 10. cant see messae history in expo app

--- MobileApp/Screens/HomeScreen.js
+++ MobileApp/Screens/HomeScreen.js
@@ -6,24 +6,33 @@ import io from "socket.io-client";
 
 export default function HomeScreen() {
   const [messageToSend, setMessageToSend] = useState("");
+  const [recvMessages, setRecvMessages] = useState([]);
   const socket = useRef(null);
 
   useEffect(() => {
     socket.current = io("http://127.0.0.1:3001");
+    socket.current.on("message", message => {
+      setRecvMessages(prevState => [...prevState, message]);
+    });
   }, []);
 
-  const sendMessaging = () => {
+  const sendMessage = () => {
     socket.current.emit("message", messageToSend);
+    setMessageToSend("");
   };
+
+  const textOfRecvMessages = recvMessages.map(msg => (
+  <Text key={msg}>{msg}</Text>
+  )); 
 
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
+      {textOfRecvMessages}
       <TextInput
         value={messageToSend}
         onChangeText= {text => setMessageToSend(text)}
-        placeholder="enter chat message..."
-        onSubmitEditing={sendMessaging}
+        placeholder="enter chat message.."
+        onSubmitEditing={sendMessage}
         />
         
         {/* <StatusBar style="auto" /> */}
