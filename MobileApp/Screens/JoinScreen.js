aa41739fe76e40a2a6bfdfd9293e4e44f09000fd
Kerim <kerim@ScorpAIs-iMac.local>
2020-12-10 16:04:26 +0000

    upgraded dependencies to; rn 0.63.4

--- MobileApp/Screens/JoinScreen.js
+++ MobileApp/Screens/JoinScreen.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text, TextInput } from "react-native"; 
+import { View, Text, TextInput, Image } from "react-native"; 
 
 export default function JoinScreen(){ 
     return {
-        // <View>
-            <Text>JoinScreen</Text>
-        // </View>
+        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+            <Image source={require("../assets/icon.png")} />
+            <TextInput placeholder="Enter username" />
+        </View>
     };
 }
