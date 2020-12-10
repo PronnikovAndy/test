0ba22932a48e64eacde86f1d1e5d4b2323d25949
Kerim <kerim@ScorpAIs-iMac.local>
2020-12-10 13:16:43 +0000

    section 5, vid 14. expo bug sending message. console log ok

--- MobileApp/Screens/HomeScreen.js
+++ MobileApp/Screens/HomeScreen.js
@@ -1,56 +1,23 @@
 import React, {useEffect, useState, useRef} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import {View} from 'react-native';
 import io from "socket.io-client";
 import { GiftedChat } from 'react-native-gifted-chat'
 
 export default function HomeScreen() {
-  const [messageToSend, setMessageToSend] = useState("");
   const [recvMessages, setRecvMessages] = useState([]);
   const socket = useRef(null);
 
   useEffect(() => {
     socket.current = io("http://127.0.0.1:3001");
     socket.current.on("message", message => {
-      const testMessage = {
-        _id: 3,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        }
-      };
-      testMessage.text = message;
-      setRecvMessages(prevState => GiftedChat.append(prevState, testMessage));
+      setRecvMessages(prevState => GiftedChat.append(prevState, message));
     });
-      setRecvMessages([
-      {
-        _id: 1,
-        text: 'Hello developer',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        }
-      },
-        {
-          _id: 2,
-          text: 'Hello from me',
-          createdAt: new Date(),
-          user: {
-            _id: 1,
-            name: 'React Native',
-            avatar: 'https://placeimg.com/140/140/any',
-          }
-        }
-    ]);
   }, []);
 
   const onSend = messages => {
     console.log(messages);
     socket.current.emit("message", messages[0].text);
+    setRecvMessages(prevState => GiftedChat.append(prevState, messages));
   };
 
   return (
@@ -63,12 +30,3 @@ export default function HomeScreen() {
       />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
